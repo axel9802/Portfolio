df657fa3a5c228ba0f10b3861ab3cf0642371414
axel9802 <axelyataco1998@gmail.com>
2021-03-18 23:16:50 -0500

    Contact Me

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+var app = document.getElementById('typing');
+ 
+var typewriter = new Typewriter(app, {
+    strings: ['Developer', 'Designer'],
+    loop: true,
+    delay: 100,
+    autoStart: true
+});
